c9130ea41d0222802c3f46a02bc85ce68465b639
Jared Szechy <jared.szechy@gmail.com>
2023-10-13 09:12:54 -0400

    mvp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Database, limitToFirst, listVal, objectVal, query, ref } from '@angular/fire/database';
+import { Observable, combineLatest, forkJoin, from, map, mergeMap, of, reduce, switchMap, tap, toArray } from 'rxjs';
+
+interface Item {
+  id: number;
+  dead?: boolean;
+  deleted?: boolean;
+  descendants?: number;
+  by: string;
+  kids: number[];
+  title: string;
+  score: number;
+  text?: string;
+  time: number;
+  type: 'story' | 'job' | 'comment' | 'poll' | 'pollopt';
+  url: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'hackernews';
+export class AppComponent implements OnInit {
+  private database: Database = inject(Database);
+  stories$: Observable<Item[]>;
+
+  constructor() {
+    const topStoriesRef = ref(this.database, '/v0/topstories');
+    const topQuery = query(topStoriesRef, limitToFirst(30));
+    this.stories$ = listVal<number>(topQuery).pipe(
+      switchMap((ids) => {
+        return combineLatest(ids.map(id => this.getItem(id))
+        )
+      }),
+      tap(() => console.log('new data'))
+    );
+  }
+
+  ngOnInit() {
+
+  }
+
+  getItem(id: number): Observable<Item> {
+    const itemRef = ref(this.database, `/v0/item/${id}`);
+    return objectVal<Item>(itemRef);
+  }
+
+  itemTrack(index: number, item: Item) {
+    return item.id;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideFirebaseApp, initializeApp, FirebaseOptions } from '@angular/fire/app';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { TimeagoModule } from 'ngx-timeago';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const fbConfig: FirebaseOptions = {
+  databaseURL: 'https://hacker-news.firebaseio.com',
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +20,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatListModule,
+    TimeagoModule.forRoot(),
+    provideFirebaseApp(() => initializeApp(fbConfig)),
+    provideDatabase(() => getDatabase())
   ],
   providers: [],
   bootstrap: [AppComponent]
