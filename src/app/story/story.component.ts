3d6253c6c119317cee0913f4bd48ea1e107193ad
Jared Szechy <jared.szechy@gmail.com>
2024-10-09 00:55:30 -0400

    score animation

--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -1,10 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { Item } from '../models/item';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-story',
   templateUrl: './story.component.html',
   styleUrls: ['./story.component.scss'],
+  animations: [
+    trigger('score', [
+      transition(':increment', animate('2s ease', keyframes([
+        style({ transform: 'scale(1)', offset: 0 }),
+        style({ transform: 'scale(1.5)', offset: 0.7 }),
+        style({ transform: 'scale(1)', offset: 1})
+      ])))
+    ]),
+  ],
 })
 export class StoryComponent {
   @Input() story!: Item;
